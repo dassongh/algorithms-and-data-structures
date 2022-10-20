24bbcaebeb9436f33405bbd7a69807ca5cd2a771
Alexander S <oleksandr.slv@gmail.com>
2022-10-20 20:53:36 +0300

    adds get and set

--- dataStructures/doubleLinkedList.js
+++ dataStructures/doubleLinkedList.js
@@ -64,11 +64,59 @@ class DoubleLinkedList {
     this.length--;
     return removedNode;
   }
+
+  unshift(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.previous = node;
+      node.next = this.head;
+      this.head = node;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+
+    const condition = index <= this.length / 2;
+    let counter = condition ? 0 : this.length - 1;
+    let value = condition ? this.head : this.tail;
+
+    while (counter !== index) {
+      value = condition ? value.next : value.previous;
+
+      if (condition) counter++;
+      else counter--;
+    }
+
+    return value;
+  }
+
+  set(value, index) {
+    const node = this.get(index);
+    if (!node) return false;
+
+    node.value = value;
+    return true;
+  }
 }
 
 const list = new DoubleLinkedList();
 
 console.log(list.push(10));
 console.log(list.push(20));
-console.log(list.shift());
-console.log(list);
+console.log(list.push(30));
+console.log(list.push(40));
+console.log(list.push(50));
+console.log(list.push(60));
+console.log(list.push(70));
+console.log(list.push(80));
+console.log(list.push(90));
+console.log(list.push(100));
+console.log('get: ', list.get(7));
